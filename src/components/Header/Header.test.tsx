2a93bb8e4d612146814a50a618699434ad9dedd0
vijayaraghavanraman <vijayaraghavan.raman@rxpservices.com>
2022-02-12 11:40:31 +1100

    Added banner component essentials

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -5,5 +5,5 @@ import { Header, HEADER_TEST_ID } from "./index";
 test("renders learn react link", () => {
   render(<Header />);
   const headerText = screen.getByTestId(HEADER_TEST_ID);
-  expect(headerText.textContent).toBe("Crypto Dashboard");
+  expect(headerText.textContent).toBe("My Portfolio");
 });
